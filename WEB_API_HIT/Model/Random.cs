864887269b6c15d87a8bc9e94a2e9d4cc270af49
ishan <ishan@DESKTOP-TOATOCU>
2023-10-05 23:03:13 +0530

    Add project files.

---
+++ WEB_API_HIT/Model/Random.cs
@@ -0,0 +1,10 @@
+﻿namespace WEB_API_HIT.Model
+{
+    public class Random
+    {
+       public string? anime {  get; set; }
+        public string? character { get; set; }   
+        public string? quote { get; set; }   
+
+    }
+}
---
+++ WEB_API_HIT/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using WEB_API_HIT.Model;
+
+namespace WEB_API_HIT.Pages
+{
+    public partial class Index
+    {
+        [Inject] HttpClient httpClient { get; set; }
+
+        public Model.Random RandomResponse = new();
+
+
+
+
+        public async Task GetData()
+        {
+            RandomResponse = await httpClient.GetFromJsonAsync<Model.Random>("https://animechan.vercel.app/api/random");
+
+
+             var obj = RandomResponse;
+
+
+        }
+    }
+}
